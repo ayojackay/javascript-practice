36a2ba9084323605a5ffc9fbaa505c3cf436c518
Jacque S <jacqueds.git@gmail.com>
2022-10-05 16:31:38 -0700

    Day 1 Take home challenge: added single line and multi-line comments and added console logs for name and favorite food item

---
+++ js/script.js
@@ -0,0 +1,63 @@
+/* COMMENTS */
+// Single Line Comment
+/*
+Multi-
+line
+comment 
+*/
+
+/**********/
+
+/* SEMICOLONS */
+// Semicolons means the ending of the line, while not required, it's considered so for best practices
+document.title = "Hello World!";
+document.title = "Hello World!"
+/********/
+
+/* CASE SENSITIVITY */
+//Pascal Case
+// FirstName
+
+//Camel Case
+// firstName
+
+// Snake Case
+// first_name
+/******/
+
+
+// console.log(firstName);
+// console.log(29);
+
+
+
+/* STRINGS */
+// Double quotes
+console.log("I'm");
+// Single quotes
+console.log('I\'m');
+// Concat
+console.log("Hello world!" + " This is awesome!");
+// String length
+console.log("Marnee".length); //6
+console.log("Marnee ".length); // Spaces count as part of the string length
+
+
+/*****/
+
+// DAY 1 Take Home
+
+// This is awesome! (single line comment)
+
+/* 
+ TODO:
+  - Finish Take home review
+  - Move on to git review
+  - Go over Primitive types
+*/
+
+console.group();
+console.log("Jacque");
+console.log("French fries");
+console.groupEnd();
+console.log("Not in a group!");
