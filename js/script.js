409e8624f3687ad72d123d1e9c4c1c52c5783bb7
Jacque S <jacqueds.git@gmail.com>
2022-10-05 16:12:28 -0700

    day 1 practice notes

---
+++ js/script.js
@@ -0,0 +1,46 @@
+/* COMMENTS */
+// Single Line Comment
+/*
+Multi-
+line
+comment 
+*/
+
+/**********/
+
+/* SEMICOLONS */
+// Semicolons means the ending of the line, while not required, it's considered so for best practices
+document.title = "Hello World!";
+document.title = "Hello World!"
+/********/
+
+/* CASE SENSITIVITY */
+//Pascal Case
+// FirstName
+
+//Camel Case
+// firstName
+
+// Snake Case
+// first_name
+/******/
+
+
+// console.log(firstName);
+// console.log(29);
+
+
+
+/* STRINGS */
+// Double quotes
+console.log("I'm");
+// Single quotes
+console.log('I\'m');
+// Concat
+console.log("Hello world!" + " This is awesome!");
+// String length
+console.log("Marnee".length); //6
+console.log("Marnee ".length); // Spaces count as part of the string length
+
+
+/*****/
